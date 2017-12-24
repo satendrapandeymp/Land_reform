af6dd4b3fcc0575d5f427b862998d1743f175c19
satyendra pandey <satendrapandeymp@gmail.com>
2017-12-25 03:19:45 +0530

    finalised

--- test.py
+++ test.py
@@ -23,13 +23,13 @@ def max(arr, flag, sector):
 def generator(x,y):
     arr = []
     row_sum = []
-    col_sum = [0,0,0,0]
+    col_sum = [0 for k in range(y)]
     temp = []    
     for j in range(x):
         for i in range(y):
             flag = randint(1,2)
-            if flag == 1 or ( i ==3 and sum(temp) == 0):
-                temp.append(randint(1,10))
+            if flag == 1 or ( i ==y-1 and sum(temp) == 0):
+                temp.append(randint(1,8))
                 col_sum[i] += temp[i] 
             else:
                 temp.append(0)
@@ -38,11 +38,13 @@ def generator(x,y):
         temp = []
     return arr, row_sum, col_sum
 
-num = 35
-sector = 4
+num = 30
+sector = 10
 temp = []
 result = []
+
 while(True):
+    print "Trying to generate random number"
     arr, row_sum, col_sum = generator(num-1,sector)
     if max(col_sum,"", sector) <= 76:
         for i in range(sector):
@@ -50,8 +52,17 @@ while(True):
         arr.append(temp)
         row_sum.append(sum(temp))
         break
+
+File_name = "Data.csv"
+f = open(File_name, "w")
+for wp in arr:
+    for wpn in wp:
+        f.write(str(wpn) + ",")  
+    f.write("\n")  
+f.close()
     
-col_sum = [76,76,76,76]
+    
+col_sum = [76 for t in range(sector)]
 
 # removing owners who have land just in one section
 owner = 0
@@ -65,15 +76,15 @@ for row in arr:
     if count == 1:
         col_sum[pos] -= row[pos] 
         row_sum[owner] = 0
-        temp = {'owner':owner, 'sector':pos, 'Amount':row[pos]}
+        temp = {'owner':owner, 'sector':pos, 'amount':row[pos]}
         row[pos] = 0
         result.append(temp)
         
     owner += 1
 
 # Now index sector wise
-index = [0,0,0,0,0]
-index_detail = [[],[],[],[],[]]
+index = [0 for t in range(sector+1)]
+index_detail = [[] for t in range(sector+1)]
 for i in range(num):
     test, check = max(arr[i], 'n', sector)
     index[check] += 1
@@ -89,7 +100,6 @@ residue = []
 checked = []
 
 while(True):
-    print col_sum, residue
     _, pos = max(index, 'n', sector)
     if pos == sector:
         break
@@ -98,9 +108,21 @@ while(True):
     temp = index_detail[pos]
     
     for i in range(len(temp)):  
+        # cross matching case
+        count = 0
+        for owner in residue:
+            if owner['amount'] == col_sum[pos]:
+                    print "You got a cross fit here"
+                    res = {'owner':owner['owner'], 'sector':pos, 'amount':owner['amount']}
+                    result.append(res)            
+                    col_sum[pos] -= owner['amount']
+                    residue.remove(residue[count])
+                    break
+            count += 1
+        
         amount = 200
         check = 0
-        count = 0
+        count = 0        
         for owner in temp:
             if owner['amount'] < amount:
                 amount = owner['amount']
@@ -108,9 +130,8 @@ while(True):
             count += 1
             
         amount = row_sum[temp[check]['owner']]
-        print amount, col_sum, pos, row_sum
         
-        if col_sum[pos] >= amount:
+        if col_sum[pos] >= amount + 10:
             
             temp[check]['amount'] = 1000
             
@@ -123,12 +144,12 @@ while(True):
                     break
             
             if opt == -1:
-                res = {'owner':temp[check]['owner'], 'sector':pos, 'Amount':amount}
+                res = {'owner':temp[check]['owner'], 'sector':pos, 'amount':amount}
                 result.append(res)            
                 col_sum[pos] -= amount
             else:
                 print "You got a fit here"
-                res = {'owner':temp[check]['owner'], 'sector':opt, 'Amount':amount}
+                res = {'owner':temp[check]['owner'], 'sector':opt, 'amount':amount}
                 result.append(res)            
                 col_sum[opt] -= amount
                 
@@ -140,7 +161,137 @@ while(True):
     checked.append(pos)
     
 # final steps
+# removing perfect fit from resude
+
+count = 0
+flag = -1
+print residue, col_sum
+for temp in residue:
+    for k in range(sector):
+        if temp['amount'] == col_sum[k]:
+            flag = count
+            col_sum[k] = 0
+            print "You got lucky here dude"
+            break
+    if flag != -1:
+        flag = -1
+        residue.remove(residue[count])
+    count += 1
+
+
 # Now we just have to take care of a few pwople
 # Will take care of this part tomorrow
-for temp in residue:
-    print temp['amount']
+col_sum_final = []
+count = 0
+for final in col_sum:
+    temp = {"sector":count, "amount":final}
+    col_sum_final.append(temp)
+    count += 1
+    
+col_sum_final = sorted(col_sum_final, key=lambda left:left['amount'])
+residue = sorted(residue, key=lambda left: left['amount'])
+
+
+print col_sum, residue
+
+flag = 0
+for i in range(len(residue)):
+    amount = residue[i-1]['amount']
+    id = residue[i-1]['owner']
+    av_amount = col_sum_final[-1]['amount']
+    sectors = col_sum_final[-1]['sector']    
+    if flag == 0:
+        flag = 1
+        if amount/3 > col_sum_final[0]['amount'] and amount/3 < col_sum_final[-1]['amount']:
+            for k in range(sector):
+                if col_sum_final[k]['amount'] > amount/3:
+                    break
+                
+            if k != sector-1:
+                if col_sum_final[-1]['amount'] - amount +  col_sum_final[k]['amount']  + col_sum_final[k-1]['amount'] < 0:
+                    k += 1
+            
+                result.append({"owner":id, "amount":amount - col_sum_final[k]['amount'] - col_sum_final[k-1]['amount'] , "sector":sectors})
+                result.append({"owner":id, "amount":col_sum_final[k]['amount'], "sector":col_sum_final[k]['sector']})
+                result.append({"owner":id, "amount":col_sum_final[k-1]['amount'], "sector":col_sum_final[k-1]['sector']})
+                residue[i-1]['amount'] = 0
+                col_sum_final[-1]['amount'] -= amount -  col_sum_final[k]['amount']  - col_sum_final[k-1]['amount']  
+                col_sum_final[k]['amount'] = 0
+                col_sum_final[k-1]['amount'] = 0       
+                
+            else:
+                
+                result.append({"owner":id, "amount":amount - col_sum_final[k-1]['amount'] - col_sum_final[k-2]['amount'] , "sector":sectors})
+                result.append({"owner":id, "amount":col_sum_final[k-1]['amount'], "sector":col_sum_final[k-1]['sector']})
+                result.append({"owner":id, "amount":col_sum_final[k-2]['amount'], "sector":col_sum_final[k-2]['sector']})
+                residue[i-1]['amount'] = 0
+                col_sum_final[-1]['amount'] -= amount -  col_sum_final[k-2]['amount']  - col_sum_final[k-1]['amount']  
+                col_sum_final[k-2]['amount'] = 0
+                col_sum_final[k-1]['amount'] = 0                 
+                    
+        elif amount/2 > col_sum_final[0]['amount'] and amount/2 < col_sum_final[-1]['amount']:
+            for k in range(sector):
+                if col_sum_final[k]['amount'] > amount/2:
+                    break
+            
+            if k == sector-1:
+                k -=1
+            
+            result.append({"owner":id, "amount":amount - col_sum_final[k]['amount'], "sector":sectors})
+            result.append({"owner":id, "amount":col_sum_final[k]['amount'], "sector":col_sum_final[k]['sector']})
+            col_sum_final[-1]['amount'] -= amount -  col_sum_final[k]['amount']  
+            residue[i-1]['amount'] = 0
+            col_sum_final[k]['amount'] = 0            
+            
+        else:
+            print 'lol lol'
+            
+    else:
+        
+        if av_amount >= amount:
+            result.append({"owner":id, "amount":amount, "sector":sectors})
+            residue[i-1]['amount'] = 0
+            col_sum_final[-1]['amount'] -= amount
+            
+        elif av_amount + col_sum_final[-2]['amount'] >= amount:
+            result.append({"owner":id, "amount":amount - col_sum_final[-2]['amount'], "sector":sectors})
+            result.append({"owner":id, "amount":col_sum_final[-2]['amount'], "sector":col_sum_final[-2]['sector']})
+            residue[i-1]['amount'] = 0
+            col_sum_final[-1]['amount'] -= amount -  col_sum_final[-2]['amount']
+            col_sum_final[-2]['amount'] = 0
+        else:
+            print 'lola'
+        
+    col_sum_final = sorted(col_sum_final, key=lambda left:left['amount'])
+
+print residue, col_sum_final
+
+result = sorted(result, key=lambda left:left['owner'])
+sol = []
+count = 0
+
+for i in range(num):
+    final = [ 0 for j in range(sector) ]
+    if result[count]['owner'] == i:
+        final[result[count]['sector']] = result[count]['amount']
+        count += 1
+    try:
+        if result[count]['owner'] == i:
+            final[result[count]['sector']] = result[count]['amount']
+            count += 1    
+        if result[count]['owner'] == i:
+            final[result[count]['sector']] = result[count]['amount']
+            count += 1
+    except:
+        a = 1
+        
+    sol.append(final)
+    
+    
+File_name = "Data1.csv"
+f = open(File_name, "w")
+for wp in sol:
+    for wpn in wp:
+        f.write(str(wpn) + ",")  
+    f.write("\n")  
+f.close()
